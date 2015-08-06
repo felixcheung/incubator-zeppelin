1b8afb94c1dbe0dd9bd71fc030f684c0ae7c487d
Felix Cheung <felixcheung_m@hotmail.com>
2015-08-06 12:06:49 -0700

    close to working SparkR interpreter

--- spark/src/main/java/org/apache/zeppelin/spark/SparkRInterpreter.java
+++ spark/src/main/java/org/apache/zeppelin/spark/SparkRInterpreter.java
@@ -11,26 +11,30 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+
 package org.apache.zeppelin.spark;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
-import java.net.ServerSocket;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.LinkedList;
 import java.util.Properties;
 
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.ddahl.rscala.java.RClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,10 +48,15 @@ import org.apache.zeppelin.interpreter.LazyOpenInterpreter;
 import org.apache.zeppelin.interpreter.WrappedInterpreter;
 import org.apache.zeppelin.interpreter.InterpreterResult.Code;
 
+/**
+ * SparkR interpreter for Zeppelin.
+ *
+ */
 public class SparkRInterpreter extends Interpreter {
   Logger logger = LoggerFactory.getLogger(SparkRInterpreter.class);
   boolean rRunning = false;
   boolean sparkRRunning = false;
+  RClient rClient = null;
 
   static {
     Interpreter.register(
@@ -83,7 +92,9 @@ public class SparkRInterpreter extends Interpreter {
 
   @Override
   public void close() {
-//
+    if (rRunning) {
+      uninitializeR();
+    }
   }
 
 
@@ -96,17 +107,19 @@ public class SparkRInterpreter extends Interpreter {
     if (!sparkRRunning) {
       SparkRConf srcf = extractSparkConf(getSparkConf());
       if (srcf == null) {
-        logger.warn("Cannot get SparkConf - is Spark interpreter running?");
+        logger.error("Cannot get SparkConf - is Spark interpreter running?");
       } else {
         try {
           initializeSparkR(srcf, getProperty("spark.home"));
+          sparkRRunning = true;
         } catch (Exception err) {
           return new InterpreterResult(Code.ERROR, "Cannot initialize SparkR");
         }
       }
     }
 
-    // TODO(felixcheung): r.eval
+    rClient.eval(st);
+    return new InterpreterResult(Code.SUCCESS, "");
   }
 
   @Override
@@ -170,49 +183,71 @@ public class SparkRInterpreter extends Interpreter {
 
   private void initializeR() {
     // Ensure R and rScala package are installed - it should exit with code 0
-    String cmdLoadLibAndCheckVer = "R -e 'library(rscala); packageVersion(\"rscala\")' --slave";
-    String rScalaVersion = execAndCapture(cmdLoadLibAndCheckVer);
-    if (!rScalaVersion.endsWith("‘1.0.6’")) {
+    String rVerOutput = execAndCapture("R --version");
+    if (!rVerOutput.startsWith("R version 3")) {
+      logger.error(rVerOutput);
+      throw new InterpreterException("R version 3 is required");
+    }
+
+    String loadLibAndCheckVerCmd = "R -e library(rscala) -e packageVersion('rscala') --slave";
+    String rScalaVersionOutput = execAndCapture(loadLibAndCheckVerCmd);
+    if (!rScalaVersionOutput.endsWith("‘1.0.6’")) {
+      logger.error(rScalaVersionOutput);
       throw new InterpreterException("rScala package version 1.0.6 is required");
     }
+
+    rClient = new RClient();
+  }
+
+  private void uninitializeR() {
+    rClient.gc();
+    rClient.exit();
   }
 
   private void initializeSparkR(SparkRConf rconf, String sparkHome) {
-    boolean loadSparkR = r.eval("require(SparkR)").asBool().isTRUE();
+    // rClient != null
+    boolean loadSparkR = rClient.evalB0("require(SparkR)");
     if (!loadSparkR) {
       throw new InterpreterException("SparkR package not installed");
     }
 
     if (sparkHome == null || sparkHome.trim().isEmpty()) {
       // try to get it from environment
-      sparkHome = System.getenv("SPARK_HOME")
+      sparkHome = System.getenv("SPARK_HOME");
     }
 
     // sparkR.init(master = "local", appName = "SparkR",
     //  sparkHome = Sys.getenv("SPARK_HOME"), sparkEnvir = list(),
     //  sparkExecutorEnv = list(), sparkJars = "", sparkRLibDir = "")
-    String sparkRInit = String.format("sc <- sparkR.init(master=\"%s\",
-    appName=\"%s\", sparkHome=\"%s\", sparkEnvir=%s, sparkJars=\"%s\")",
-    rconf.sparkMaster, "zeppelin-SparkR", sparkHome, rconf.sparkEnvir, rconf.sparkJars);
-    r.eval(sparkRInit);
+    String sparkRInit = String.format("sc <- sparkR.init(master=\"%s\", "
+      + "appName=\"%s\", sparkHome=\"%s\", sparkEnvir=%s, sparkJars=\"%s\")",
+      rconf.sparkMaster, "zeppelin-SparkR", sparkHome, rconf.sparkEnvir, rconf.sparkJars);
+    logger.info(String.format("Initializing SparkR with: %s", sparkRInit));
+
+    // Initialize SparkR
+    rClient.eval(sparkRInit);
   }
 
   // Get SparkConf from the existing SparkContext into R code
   // eg. list(spark.executor.memory="1g", spark.master="local[*]")
   private SparkRConf extractSparkConf(SparkConf scf) {
     // Exclude these Spark config value since they should be unique per driver
-    Map excludeConf = new HashSet();
+    HashSet excludeConf = new HashSet();
     excludeConf.add("spark.app.id");
     excludeConf.add("spark.app.name");
     excludeConf.add("spark.driver.port");
     excludeConf.add("spark.tachyonStore.folderName");
     excludeConf.add("spark.repl.class.uri");
     excludeConf.add("spark.fileserver.uri");
+    excludeConf.add("zeppelin.spark.useHiveContext");
+    excludeConf.add("zeppelin.spark.concurrentSQL")
+    excludeConf.add("zeppelin.pyspark.python")
+    excludeConf.add("zeppelin.dep.localrepo")
 
     SparkRConf rconf = new SparkRConf();
     boolean first = true;
     StringBuffer sb = new StringBuffer("list(");
-    for (scala.Tuple2<String,String> keyValue : scf.getAll()) {
+    for (scala.Tuple2<String, String> keyValue : scf.getAll()) {
       // Trim should not be necessary for the key, but let's be safe
       String key = keyValue._1().trim();
       String value = keyValue._2().trim();
@@ -227,6 +262,7 @@ public class SparkRInterpreter extends Interpreter {
           sb.append(", ");
         }
         sb.append(String.format("%s=\"%s\"", key, value));
+        first = false;
       }
     }
     sb.append(")");
@@ -251,25 +287,26 @@ public class SparkRInterpreter extends Interpreter {
         output.add(line);
       }
       input.close();
-      p.exitValue();
+      exitValue = p.waitFor();
     }
     catch (Exception err) {
       err.printStackTrace();
       if (required) {
         logger.error(err.toString());
-        throw err;
+        throw new InterpreterException(err);
       }
       logger.warn(err.toString());
     }
     String result = "";
     if (exitValue == 0) {
       // flatten array and remove empty line and whitespace.
-      result = StringUtils.trim(StringUtils.chomp(StringUtils.join(output, '\n')));
+      String separators = System.getProperty("line.separator");
+      result = StringUtils.trim(StringUtils.chomp(StringUtils.join(output, separators)));
     }
     if (StringUtils.isBlank(result)) {
       if (required) {
         String errorMsg = String.format("Command %s not completed successfully.", cmd);
-        logger.error(errorMsg)
+        logger.error(errorMsg);
         throw new InterpreterException(errorMsg);
       } else {
         result = "";
